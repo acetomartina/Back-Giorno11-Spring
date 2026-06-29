1e19b8e7f1ba98330c2c53710b9d07e4153c346b
Martina <acetomartina141@gmail.com>
2026-06-29 16:00:14 +0200

    MenuConfig-ok

---
+++ src/main/java/config/MenuConfig.java
@@ -0,0 +1,166 @@
+package config;
+
+import model.Drink;
+import model.Menu;
+import model.Pizza;
+import model.Topping;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class MenuConfig {
+
+    // TOPPING
+
+    @Bean
+    public Topping pomodoro() {
+        return new Topping("Pomodoro", 0.00, 20);
+    }
+
+    @Bean
+    public Topping mozzarella() {
+        return new Topping("Mozzarella", 1.00, 120);
+    }
+
+    @Bean
+    public Topping prosciuttoCotto() {
+        return new Topping("Prosciutto cotto", 1.50, 90);
+    }
+
+    @Bean
+    public Topping ananas() {
+        return new Topping("Ananas", 1.20, 50);
+    }
+
+    @Bean
+    public Topping funghi() {
+        return new Topping("Funghi", 1.20, 30);
+    }
+
+    @Bean
+    public Topping salamePiccante() {
+        return new Topping("Salame piccante", 1.80, 160);
+    }
+
+    @Bean
+    public Topping olive() {
+        return new Topping("Olive", 1.00, 40);
+    }
+
+    @Bean
+    public Topping wurstel() {
+        return new Topping("Würstel", 1.50, 140);
+    }
+
+    @Bean
+    public Topping patatine() {
+        return new Topping("Patatine", 1.50, 180);
+    }
+
+    // DRINK
+
+    @Bean
+    public Drink cocaCola() {
+        return new Drink("Coca Cola", 2.50, 150);
+    }
+
+    @Bean
+    public Drink fanta() {
+        return new Drink("Fanta", 2.50, 140);
+    }
+
+    @Bean
+    public Drink acquaNaturale() {
+        return new Drink("Acqua naturale", 1.50, 0);
+    }
+
+    @Bean
+    public Drink birra() {
+        return new Drink("Birra", 4.00, 180);
+    }
+
+    // PIZZE
+
+    @Bean
+    public Pizza margherita() {
+        return new Pizza(
+                "Margherita",
+                List.of(pomodoro(), mozzarella())
+        );
+    }
+
+    @Bean
+    public Pizza hawaiiana() {
+        return new Pizza(
+                "Hawaiiana",
+                List.of(pomodoro(), mozzarella(), prosciuttoCotto(), ananas())
+        );
+    }
+
+    @Bean
+    public Pizza doppioProsciutto() {
+        return new Pizza(
+                "Doppio prosciutto",
+                List.of(pomodoro(), mozzarella(), prosciuttoCotto(), prosciuttoCotto())
+        );
+    }
+
+    @Bean
+    public Pizza diavola() {
+        return new Pizza(
+                "Diavola",
+                List.of(pomodoro(), mozzarella(), salamePiccante())
+        );
+    }
+
+    @Bean
+    public Pizza capricciosa() {
+        return new Pizza(
+                "Capricciosa",
+                List.of(pomodoro(), mozzarella(), prosciuttoCotto(), funghi(), olive())
+        );
+    }
+
+    @Bean
+    public Pizza americana() {
+        return new Pizza(
+                "Americana",
+                List.of(pomodoro(), mozzarella(), wurstel(), patatine())
+        );
+    }
+
+    // MENU
+
+    @Bean
+    public Menu menu() {
+        return new Menu(
+                List.of(
+                        margherita(),
+                        hawaiiana(),
+                        doppioProsciutto(),
+                        diavola(),
+                        capricciosa(),
+                        americana()
+                ),
+                List.of(
+                        pomodoro(),
+                        mozzarella(),
+                        prosciuttoCotto(),
+                        ananas(),
+                        funghi(),
+                        salamePiccante(),
+                        olive(),
+                        wurstel(),
+                        patatine()
+                ),
+                List.of(
+                        cocaCola(),
+                        fanta(),
+                        acquaNaturale(),
+                        birra()
+                )
+        );
+    }
+}
--- src/main/java/model/Pizza.java
+++ src/main/java/model/Pizza.java
@@ -5,16 +5,36 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @Setter
 @NoArgsConstructor
-public class Pizza extends MenuItem {
+public class Pizza {
 
+    private String name;
     private List<Topping> toppings;
 
-    public Pizza(String name, double price, int calories, List<Topping> toppings) {
-        super(name, price, calories);
+    public Pizza(String name, List<Topping> toppings) {
+        this.name = name;
         this.toppings = toppings;
+    }
+
+    public double getPrice() {
+        return toppings.stream()
+                .mapToDouble(Topping::getPrice)
+                .sum();
+    }
+
+    public int getCalories() {
+        return toppings.stream()
+                .mapToInt(Topping::getCalories)
+                .sum();
+    }
+
+    public String getToppingsString() {
+        return toppings.stream()
+                .map(Topping::getName)
+                .collect(Collectors.joining(", "));
     }
 }
